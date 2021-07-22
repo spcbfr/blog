b4376395730e58d9495fe908dd5cc52af62ef3ad
YoussefBouzekri <youssefbouzekri@protonmail.com>
2021-07-22 12:00:56 +0100

    update more of my name all over the website

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ export const About = (): JSX.Element => {
   return (
     <Layout
       customMeta={{
-        title: 'About - Hunter Chang',
+        title: 'About Me - Youssef Bouzekri',
       }}
     >
       <h1>About Page</h1>
